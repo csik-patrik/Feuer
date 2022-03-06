67f3152dae3bd56902ae3b85be56e4f4424b6db6
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-03-06 11:22:39 +0100

    created migration for courses table according to database plan

--- FeuerMoodle/database/migrations/2022_03_06_100734_create_courses_table.php
+++ FeuerMoodle/database/migrations/2022_03_06_100734_create_courses_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('courses', function (Blueprint $table) {
-            $table->id();
+            $table->inrements('course_id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->foreign('category_id')->references('category_id')->on('course_categories');
+            $table->foreign('owner_id')->references('user_id')->on('course_categories');
             $table->timestamps();
         });
     }
