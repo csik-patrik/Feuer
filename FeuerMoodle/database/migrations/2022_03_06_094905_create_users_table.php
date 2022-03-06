f9f956201d1ab92d111d91632990a97bd06a90b8
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-03-06 10:54:16 +0100

    created users table migration according to database plan

--- FeuerMoodle/database/migrations/2022_03_06_094905_create_users_table.php
+++ FeuerMoodle/database/migrations/2022_03_06_094905_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('user_id');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->foreign('role_id')->references('role_id')->on('roles');
             $table->timestamps();
         });
     }
