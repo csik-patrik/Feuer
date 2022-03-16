7a6a4135bdb4ce7811e32b56d4c8710f7d9e41b2
Patrik Csik <csik.patrik.w@gmail.com>
2022-03-16 16:59:21 +0100

    feat: add index method

--- FeuerMoodle/app/Http/Controllers/CourseController.php
+++ FeuerMoodle/app/Http/Controllers/CourseController.php
@@ -14,7 +14,10 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        $courses = Course::latest()->paginate(5);
+    
+        return view('courses.index',compact('courses'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
