bdf92d6440e2625359d151c864fc74b7bde4a581
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-03-20 15:04:23 +0100

    created routes for login

--- FeuerMoodle/routes/web.php
+++ FeuerMoodle/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('loginAuth', [AuthController::class, 'login'])->name('login.auth');
+Route::get('main', [AuthController::class, 'showMainPage'])->name('main');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
