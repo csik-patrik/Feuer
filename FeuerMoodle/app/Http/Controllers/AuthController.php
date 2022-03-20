a5de099f8a68b43903ca3516ca43bcca33f6b078
SzaboGergo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-03-20 14:50:37 +0100

    created auth controller

---
+++ FeuerMoodle/app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function showMainPage()
+    {
+        
+    }
+
+    public function logout()
+    {
+
+    }
+
+}
