7de3887dcf67a1350319beb1541eaa0d4cfc7f7e
oraveczaron <oraveczaron68@gmail.com>
2022-03-21 19:23:21 +0100

    using UsersController, added /users Route

--- FeuerMoodle/routes/web.php
+++ FeuerMoodle/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,5 @@ Route::get('main', [AuthController::class, 'showMainPage'])->name('main');
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::resource('/courses', CourseController::class);
+Route::resource('/users', UserController::class);
 
