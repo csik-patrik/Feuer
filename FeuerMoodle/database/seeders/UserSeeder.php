4c76048604b255f5443870ce851c2f4dc6a205af
GergoSzabo00 <YGJWWN@std.uni-eszterhazy.hu>
2022-03-22 11:24:36 +0100

    created user seeder

---
+++ FeuerMoodle/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'role_id' => 0
+        ]);
+    }
+}
