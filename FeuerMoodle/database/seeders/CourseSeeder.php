90e72115c2a05cf52dcfb2881d2c129315fea1ee
csik-patrik <csik.patrik455@gmail.com>
2022-03-27 15:45:26 +0200

    feat: add courses to seeder

--- FeuerMoodle/database/seeders/CourseSeeder.php
+++ FeuerMoodle/database/seeders/CourseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Course;
 use Illuminate\Database\Seeder;
 
 class CourseSeeder extends Seeder
@@ -14,6 +14,29 @@ class CourseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Course::create([
+            'code' => '8E1NKM6N',
+            'name' => 'Webprogramozás 3',
+            'category_id' => '1',
+            'owner_id' => '1'
+        ]);
+        Course::create([
+            'code' => '97MKNC01',
+            'name' => 'Magasszintű programozási nyelvek 2',
+            'category_id' => '1',
+            'owner_id' => '1'
+        ]);
+        Course::create([
+            'code' => 'W4ZQEHOK',
+            'name' => 'Robotika',
+            'category_id' => '1',
+            'owner_id' => '1'
+        ]);
+        Course::create([
+            'code' => '1FF8SFSC',
+            'name' => 'Algoritmusok 2',
+            'category_id' => '1',
+            'owner_id' => '1'
+        ]);
     }
 }
