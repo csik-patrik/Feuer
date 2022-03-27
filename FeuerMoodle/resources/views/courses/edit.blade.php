a588c6f9a2a3fdede6c6fee907f80db53128d8db
csik-patrik <csik.patrik455@gmail.com>
2022-03-27 15:25:41 +0200

    fix: changed form elmenets to hungarian

--- FeuerMoodle/resources/views/courses/edit.blade.php
+++ FeuerMoodle/resources/views/courses/edit.blade.php
@@ -35,26 +35,26 @@
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Code:</strong>
-                    <input type="text" value="{{$course['code']}}" name="code" class="form-control" placeholder="Course code">
+                    <strong>Kurzus kód:</strong>
+                    <input type="text" value="{{$course['code']}}" name="code" class="form-control" placeholder="Kurzus kód">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Name:</strong>
-                    <input type="text" value="{{$course['name']}}" name="name" class="form-control" placeholder="Name">
+                    <strong>Név:</strong>
+                    <input type="text" value="{{$course['name']}}" name="name" class="form-control" placeholder="Név">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Caregory Id:</strong>
-                    <input type="number" name="categoryId" value="{{$course['category_id']}}" class="form-control" placeholder="Category Id">
+                    <strong>Kategória Id:</strong>
+                    <input type="number" name="categoryId" value="{{$course['category_id']}}" class="form-control" placeholder="Kategória Id">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Owner Id:</strong>
-                    <input type="number " value="{{$course['owner_id']}}" name="ownerId" class="form-control" placeholder="Owner Id">
+                    <strong>Tulajdonos Id:</strong>
+                    <input type="number " value="{{$course['owner_id']}}" name="ownerId" class="form-control" placeholder="Tulajdonos Id">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
--- FeuerMoodle/resources/views/courses/show.blade.php
+++ FeuerMoodle/resources/views/courses/show.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Kurzus megjelenítése</h2>
+                <h2> Kurzus részletek</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('courses.index') }}"> Vissza</a>
@@ -21,26 +21,26 @@
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Code:</strong>
-                <input type="text" disabled value="{{$course['code']}}" name="code" class="form-control" placeholder="Course code">
+                <strong>Kód:</strong>
+                <input type="text" disabled value="{{$course['code']}}" name="code" class="form-control" placeholder="Kurzus kód">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" disabled value="{{$course['name']}}" name="name" class="form-control" placeholder="Name">
+                <strong>Név:</strong>
+                <input type="text" disabled value="{{$course['name']}}" name="name" class="form-control" placeholder="Név">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Caregory Id:</strong>
-                <input type="number" disabled name="categoryId" value="{{$course['category_id']}}" class="form-control" placeholder="Category Id">
+                <strong>Kategória Id:</strong>
+                <input type="number" disabled name="categoryId" value="{{$course['category_id']}}" class="form-control" placeholder="Kategória Id">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Owner Id:</strong>
-                <input type="number " disabled value="{{$course['owner_id']}}" name="ownerId" class="form-control" placeholder="Owner Id">
+                <strong>Tulajdonos Id:</strong>
+                <input type="number " disabled value="{{$course['owner_id']}}" name="ownerId" class="form-control" placeholder="Tulajdonos Id">
             </div>
         </div>
         
