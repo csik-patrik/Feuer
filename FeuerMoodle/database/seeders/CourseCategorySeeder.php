7dfaafe43d586a6e812f39ed43151a13f86833e6
csik-patrik <csik.patrik455@gmail.com>
2022-03-27 15:38:23 +0200

    feat: add courseCategory seeder

---
+++ FeuerMoodle/database/seeders/CourseCategorySeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CourseCategory;
+use Illuminate\Database\Seeder;
+
+class CourseCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
