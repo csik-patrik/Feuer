dfe2d5c23d9a6d9e0bc27e46763d5fbb1daf5987
csik-patrik <csik.patrik455@gmail.com>
2022-04-05 08:57:38 +0200

    add admin route, cleanup

--- FeuerMoodle/routes/web.php
+++ FeuerMoodle/routes/web.php
@@ -5,22 +5,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('loginAuth', [AuthController::class, 'login'])->name('login.auth');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
-Route::post('loginAuth', [AuthController::class, 'login'])->name('login.auth');
 Route::get('main', [AuthController::class, 'showMainPage'])->name('main');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::resource('/courses', CourseController::class);
 Route::resource('/users', UserController::class);
 
+Route::get('/admin', function () {
+    return View('adminPage.index');
+});
