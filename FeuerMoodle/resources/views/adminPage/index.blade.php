323fafdf2c3ee9888f62fb24b6f388246e481eaa
csik-patrik <csik.patrik455@gmail.com>
2022-04-05 10:42:37 +0200

    add more cards

--- FeuerMoodle/resources/views/adminPage/index.blade.php
+++ FeuerMoodle/resources/views/adminPage/index.blade.php
@@ -1,32 +1,54 @@
 @extends('adminPage.layout')
 @section('content')
-<div class="container">
     <div class="row">
         <div class="col-lg-12 margin-tb text-center p-2">
             <div class="">
                 <h2>Adminisztráció</h2>
             </div>
         </div>
-        <div class="col-lg-6 margin-tb">
+    </div>
+    <div class="row">
+        <div class="col-lg-4 margin-tb">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Felhasználók kép">
+                <img class="card-img-top" src="url(../images/logo.svg)" alt="Felhasználók">
                 <div class="card-body">
                   <h5 class="card-title">Felhasználók</h5>
                   <p class="card-text">Felhasználók menedzselése: Felhasználó megtekintése hozzáadása, módosítása, törlése.</p>
                   <a href="{{ url("/courses") }}" class="btn btn-primary">Megnyitás</a>
                 </div>
-              </div>
+            </div>
         </div>
-        <div class="col-lg-6 margin-tb">
+        <div class="col-lg-4 margin-tb">
             <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Kurzusok kép">
+                <img class="card-img-top" src="..." alt="Kurzusok">
                 <div class="card-body">
                   <h5 class="card-title">Kurzusok</h5>
                   <p class="card-text">Kurzusok menedzselése: Kurzus megtekintése hozzáadása, módosítása, törlése.</p>
                   <a href="{{ url("/users") }}" class="btn btn-primary">Megnyitás</a>
                 </div>
-              </div>
+            </div>
+        </div>
+        <div class="col-lg-4 margin-tb">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Kurzus kategórák">
+                <div class="card-body">
+                  <h5 class="card-title">Kurzus kategóriák</h5>
+                  <p class="card-text">Kurzusok kategóriák menedzselése: Kurzus kategóriák megtekintése hozzáadása, módosítása, törlése.</p>
+                  <a href="#" class="btn btn-primary">Megnyitás</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col-lg-4 margin-tb">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Kurzus kategórák">
+                <div class="card-body">
+                  <h5 class="card-title">Kurzus tagok</h5>
+                  <p class="card-text">Kurzusok tagok menedzselése: Kurzus tagok megtekintése hozzáadása, módosítása, törlése.</p>
+                  <a href="#" class="btn btn-primary">Megnyitás</a>
+                </div>
+            </div>
+        </div>
+    </div>        
 @endsection
