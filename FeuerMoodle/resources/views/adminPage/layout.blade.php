63c9851b28c881703e9c1ece7fb29d0caf12823d
csik-patrik <csik.patrik455@gmail.com>
2022-04-05 08:25:21 +0200

    include header footer

--- FeuerMoodle/resources/views/adminPage/layout.blade.php
+++ FeuerMoodle/resources/views/adminPage/layout.blade.php
@@ -5,8 +5,10 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
     </head>
     <body>
+        @include('./_navbar')
         <div class="container">
             @yield('content')
         </div>
+        @include('./_footer')
     </body>
 </html>
