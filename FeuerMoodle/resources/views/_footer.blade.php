b660bf65324f62ac0036c45aff949a85f14773f8
csik-patrik <csik.patrik455@gmail.com>
2022-04-05 10:57:07 +0200

    add mt-auto class to footer

--- FeuerMoodle/resources/views/_footer.blade.php
+++ FeuerMoodle/resources/views/_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-secondary">
+<footer class="bg-secondary mt-auto">
     <div class="container">
         <div class="text-center p-3">
             Copyright &copy; {{ now()->year }} {{ config('app.name') }}
