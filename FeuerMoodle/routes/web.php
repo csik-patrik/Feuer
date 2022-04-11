f544525ae85890e58602d0a6b6a9823db2b33e5c
csik-patrik <csik.patrik455@gmail.com>
2022-04-11 21:01:02 +0200

    remove auth routes

--- FeuerMoodle/routes/web.php
+++ FeuerMoodle/routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
 
-Route::post('loginAuth', [AuthController::class, 'login'])->name('login.auth');
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::get('main', [AuthController::class, 'showMainPage'])->name('main');
 
 Route::resource('/courses', CourseController::class);
