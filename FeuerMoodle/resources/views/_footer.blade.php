ce1535d8322cf43e5ff363b64aa1396e26e1168c
oraveczaron <oraveczaron68@gmail.com>
2022-04-12 15:00:39 +0200

    moved to layouts folder

--- FeuerMoodle/resources/views/_footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="bg-secondary">
-    <div class="container">
-        <div class="text-center p-3">
-            Copyright &copy; {{ now()->year }} {{ config('app.name') }}
-        </div>
-    </div>
-</footer>
