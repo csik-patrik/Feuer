752fc5813700c12f704d9c27b925e9b8e6918f0d
csik-patrik <csik.patrik455@gmail.com>
2022-04-12 14:26:02 +0200

    fix: responsive

--- FeuerMoodle/resources/views/adminPage/index.blade.php
+++ FeuerMoodle/resources/views/adminPage/index.blade.php
@@ -1,54 +1,54 @@
 @extends('adminPage.layout')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb text-center p-2">
-            <div class="">
-                <h2>Adminisztráció</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 margin-tb text-center p-2">
+                <div class="">
+                    <h2>Adminisztráció</h2>
+                </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12 margin-tb">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top mt-3" height="150" src="{{ url('..\images\user.svg') }}" alt="Felhasználók">
+                    <div class="card-body">
+                      <h5 class="card-title">Felhasználók</h5>
+                      <p class="card-text">Felhasználók menedzselése: Felhasználó megtekintése hozzáadása, módosítása, törlése.</p>
+                      <a href="{{ url("/users") }}" class="btn btn-primary">Megnyitás</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 margin-tb">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" height="150" src="{{ url('..\images\course.svg') }}" alt="Kurzusok">
+                    <div class="card-body">
+                      <h5 class="card-title">Kurzusok</h5>
+                      <p class="card-text">Kurzusok menedzselése: Kurzus megtekintése hozzáadása, módosítása, törlése.</p>
+                      <a href="{{ url("/courses") }}" class="btn btn-primary">Megnyitás</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 margin-tb">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top mt-3" height="150" src="{{ url('..\images\course_category.svg') }}" alt="Kurzus kategórák">
+                    <div class="card-body">
+                      <h5 class="card-title">Kurzus kategóriák</h5>
+                      <p class="card-text">Kurzusok kategóriák menedzselése: Kurzus kategóriák megtekintése hozzáadása, módosítása, törlése.</p>
+                      <a href="#" class="btn btn-primary">Megnyitás</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 margin-tb">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" height="150" src="{{ url('..\images\course_members.svg') }}" alt="Kurzus kategórák">
+                    <div class="card-body">
+                      <h5 class="card-title">Kurzus tagok</h5>
+                      <p class="card-text">Kurzusok tagok menedzselése: Kurzus tagok megtekintése hozzáadása, módosítása, törlése.</p>
+                      <a href="#" class="btn btn-primary">Megnyitás</a>
+                    </div>
+                </div>
+            </div>
+        </div>        
     </div>
-    <div class="row">
-        <div class="col-lg-4 margin-tb">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top mt-3" height="150" src="{{ url('..\images\user.svg') }}" alt="Felhasználók">
-                <div class="card-body">
-                  <h5 class="card-title">Felhasználók</h5>
-                  <p class="card-text">Felhasználók menedzselése: Felhasználó megtekintése hozzáadása, módosítása, törlése.</p>
-                  <a href="{{ url("/users") }}" class="btn btn-primary">Megnyitás</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 margin-tb">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" height="150" src="{{ url('..\images\course.svg') }}" alt="Kurzusok">
-                <div class="card-body">
-                  <h5 class="card-title">Kurzusok</h5>
-                  <p class="card-text">Kurzusok menedzselése: Kurzus megtekintése hozzáadása, módosítása, törlése.</p>
-                  <a href="{{ url("/courses") }}" class="btn btn-primary">Megnyitás</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 margin-tb">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top mt-3" height="150" src="{{ url('..\images\course_category.svg') }}" alt="Kurzus kategórák">
-                <div class="card-body">
-                  <h5 class="card-title">Kurzus kategóriák</h5>
-                  <p class="card-text">Kurzusok kategóriák menedzselése: Kurzus kategóriák megtekintése hozzáadása, módosítása, törlése.</p>
-                  <a href="#" class="btn btn-primary">Megnyitás</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-4 margin-tb">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" height="150" src="{{ url('..\images\course_members.svg') }}" alt="Kurzus kategórák">
-                <div class="card-body">
-                  <h5 class="card-title">Kurzus tagok</h5>
-                  <p class="card-text">Kurzusok tagok menedzselése: Kurzus tagok megtekintése hozzáadása, módosítása, törlése.</p>
-                  <a href="#" class="btn btn-primary">Megnyitás</a>
-                </div>
-            </div>
-        </div>
-    </div>        
 @endsection
