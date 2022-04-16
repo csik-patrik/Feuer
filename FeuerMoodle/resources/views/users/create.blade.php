d21472eb53a0f66527667377678cbaa7441d14c9
csik-patrik <csik.patrik455@gmail.com>
2022-04-16 09:16:12 +0200

    add container

--- FeuerMoodle/resources/views/users/create.blade.php
+++ FeuerMoodle/resources/views/users/create.blade.php
@@ -1,59 +1,61 @@
 @extends('layouts.layout')
   
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Felhasználó hozzáadása</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Felhasználó hozzáadása</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('users.index') }}"> Vissza</a>
+                </div>
+            </div>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Vissza</a>
-        </div>
+           
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Hiba!</strong> Hibás bemeneti adatok!<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+           
+        <form action="{{ route('users.store') }}" method="POST">
+            @csrf
+          
+             <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Id:</strong>
+                        <input type="number" name="id" class="form-control" placeholder="Id">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Felhasználónév:</strong>
+                        <input type="text" name="username" class="form-control" placeholder="Felhasználónév">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Jelszó:</strong>
+                        <input type="text" name="password" class="form-control" placeholder="Jeszló">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Jogosultság Id:</strong>
+                        <input type="number" name="roleId" class="form-control" placeholder="Jogosultság Id">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary">Hozzáadás</button>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
-   
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Hiba!</strong> Hibás bemeneti adatok!<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-   
-<form action="{{ route('users.store') }}" method="POST">
-    @csrf
-  
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Id:</strong>
-                <input type="number" name="id" class="form-control" placeholder="Id">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Felhasználónév:</strong>
-                <input type="text" name="username" class="form-control" placeholder="Felhasználónév">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Jelszó:</strong>
-                <input type="text" name="password" class="form-control" placeholder="Jeszló">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Jogosultság Id:</strong>
-                <input type="number" name="roleId" class="form-control" placeholder="Jogosultság Id">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Hozzáadás</button>
-        </div>
-    </div>
-</form>
 @endsection
