d23111d4009d941fcc7ea6f349222bb2b2a967dc
csik-patrik <csik.patrik455@gmail.com>
2022-04-16 15:23:54 +0200

    Change table, add new fields

--- FeuerMoodle/resources/views/users/show.blade.php
+++ FeuerMoodle/resources/views/users/show.blade.php
@@ -1,30 +1,46 @@
 @extends('layouts.layout')
-@section('title') {{$user['username']}} @endsection
+@section('title') {{$user['username']}} részletek @endsection
 @section('content')
     <div class="container p-3">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Felhasználó részeleti:</h2>
-                </div>
-                <div class="pull-right">
-                    <a class="btn btn-primary" href="{{ route('users.index') }}"> Vissza</a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Felhasználónév:</strong>
-                    <input type="text" disabled value="{{$user['username']}}" name="username" class="form-control" placeholder="Felhasználónév">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Szerepkör:</strong>
-                    <input type="number" disabled name="roleId" class="form-control" placeholder="{{$user->role->role_name}}">
-                </div>
-            </div>
-        </div>
+        <table class="table  table-striped">
+            <thead class="thead-dark">
+                <tr class="row">
+                    <th class="col-lg-11 margin-tb" scope="col">Felhasználó részeleti:</th>
+                    <th class="col-lg-1 margin-tb" scope="col"><a class="btn btn-primary" href="{{ route('users.index') }}"> Vissza</a></th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="row">
+                    <th class="col-lg-6 col-md-6 col-sm-12" scope="row">Felhasználónév:</th>
+                    <td class="col-lg-6 col-md-6 col-sm-12">{{$user->username}}</td>
+                </tr>
+                <tr class="row">
+                    <th class="col-lg-6 col-md-6 col-sm-12" scope="row">Teljes név:</th>
+                    @if($user->userData->midname != null)
+                        <td class="col-lg-6 col-md-6 col-sm-12">
+                            {{$user->userData->lastname}} {{$user->userData->midname}} 
+                                {{$user->userData->firstname}}
+                        </td>
+                    @else
+                    <td class="col-lg-6 col-md-6 col-sm-12">
+                        {{$user->userData->lastname}} {{$user->userData->firstname}}
+                    </td>
+                    @endif
+                </tr>
+                <tr class="row">
+                    <th class="col-lg-6 col-md-6 col-sm-12" scope="row">Szerepkör:</th>
+                    <td class="col-lg-6 col-md-6 col-sm-12" >{{$user->role->role_name}}</td>
+                </tr>
+                <tr class="row">
+                    <th class="col-lg-6 col-md-6 col-sm-12" scope="row">E-mail cím:</th>
+                    <td class="col-lg-6 col-md-6 col-sm-12">{{$user->userData->email}}</td>
+                </tr>
+                <tr class="row">
+                    <th class="col-lg-6 col-md-6 col-sm-12" scope="row">Telefonszám:</th>
+                    <td class="col-lg-6 col-md-6 col-sm-12">{{$user->userData->phone}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
