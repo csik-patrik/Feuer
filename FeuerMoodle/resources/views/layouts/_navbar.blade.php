dd6d46e9b238eeea6a212aee7f91f76f0c710968
csik-patrik <csik.patrik455@gmail.com>
2022-04-16 08:44:53 +0200

    feat: add admin page link

--- FeuerMoodle/resources/views/layouts/_navbar.blade.php
+++ FeuerMoodle/resources/views/layouts/_navbar.blade.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,9 +7,13 @@
         <li class="nav-item active">
           <a class="nav-link" href="#">Kezdőlap</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Felhasználók</a>
-        </li>
+        @auth
+            @if(Auth::user()->role->role_id==1)
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/admin')}}" >Adminisztráció</a>
+              </li>
+            @endif
+        @endauth
         <li class="nav-item">
           <a class="nav-link" href="{{url('/user/courses')}}" >Kurzusok</a>
         </li>
@@ -26,13 +29,3 @@
       </ul>
     </div>
   </nav>
-
-        <!-- 
-        Css link:
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-        Js links:
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        -->
