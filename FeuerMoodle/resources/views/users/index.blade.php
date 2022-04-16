a132eb6b23586e260d77ebdb1ce94a1c9ecea6fb
csik-patrik <csik.patrik455@gmail.com>
2022-04-16 09:06:28 +0200

    add container

--- FeuerMoodle/resources/views/users/index.blade.php
+++ FeuerMoodle/resources/views/users/index.blade.php
@@ -1,52 +1,54 @@
 @extends('layouts.layout')
  
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Felhasználók listázása</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('users.create') }}"> Új felhasználó hozzáadása</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Felhasználók listázása</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-success" href="{{ route('users.create') }}"> Új felhasználó hozzáadása</a>
+                </div>
             </div>
         </div>
+       
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+       
+        <table class="table table-bordered">
+            <tr>
+                <th>Felhasználónév</th>
+                <th>Jelszó</th>
+                <th>Jogosultság</th>
+                <th>Művelet</th>
+            </tr>
+            @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->password }}</td>
+                <td>{{ $user->role->role_name }}</td>
+                <td>
+                    <form action="{{ route('users.destroy',$user->user_id) }}" method="POST">
+       
+                        <a class="btn btn-info" href="{{ route('users.show',$user->user_id) }}">Részletek</a>
+        
+                        <a class="btn btn-primary" href="{{ route('users.edit',$user->user_id) }}">Módosítás</a>
+       
+                        @csrf
+                        @method('DELETE')
+          
+                        <button type="submit" class="btn btn-danger">Törlés</button>
+                    </form>
+                </td>
+                
+            </tr>
+            @endforeach
+        </table>
     </div>
-   
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-   
-    <table class="table table-bordered">
-        <tr>
-            <th>Felhasználónév</th>
-            <th>Jelszó</th>
-            <th>Jogosultság</th>
-            <th>Művelet</th>
-        </tr>
-        @foreach ($users as $user)
-        <tr>
-            <td>{{ $user->username }}</td>
-            <td>{{ $user->password }}</td>
-            <td>{{ $user->role->role_name }}</td>
-            <td>
-                <form action="{{ route('users.destroy',$user->user_id) }}" method="POST">
-   
-                    <a class="btn btn-info" href="{{ route('users.show',$user->user_id) }}">Részletek</a>
-    
-                    <a class="btn btn-primary" href="{{ route('users.edit',$user->user_id) }}">Módosítás</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger">Törlés</button>
-                </form>
-            </td>
-            
-        </tr>
-        @endforeach
-    </table>
   
     
       
