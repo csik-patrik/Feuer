05c071677a82c795c2556c0abb97ce80272644d2
csik-patrik <csik.patrik455@gmail.com>
2022-04-16 09:55:51 +0200

    add roles to view

--- FeuerMoodle/app/Http/Controllers/UserController.php
+++ FeuerMoodle/app/Http/Controllers/UserController.php
@@ -80,7 +80,8 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        return view('users.edit', compact('user'));
+        $roles = Role::orderBy('role_name')->get();
+        return view('users.edit', compact('user', 'roles'));
     }
 
     /**
