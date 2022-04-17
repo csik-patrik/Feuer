6b7715eff54015d19893e9489fa961e31cc3d020
csik-patrik <csik.patrik455@gmail.com>
2022-04-17 08:16:50 +0200

    Update table

--- FeuerMoodle/resources/views/users/index.blade.php
+++ FeuerMoodle/resources/views/users/index.blade.php
@@ -1,35 +1,30 @@
 @extends('layouts.layout')
- 
+@section('title')Felhasználók @endsection
 @section('content')
-    <div class="container p-3">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Felhasználók listázása</h2>
-                </div>
-                <div class="pull-right">
-                    <a class="btn btn-success" href="{{ route('users.create') }}"> Új felhasználó hozzáadása</a>
-                </div>
-            </div>
-        </div>
-       
+<div class="container p-3">
+    <table class="table  table-striped">
+        <thead class="thead-dark">
+            <tr class="row">
+                <th class="col-lg-9 margin-tb" scope="col">Felhasználók:</th>
+                <th class="col-lg-3 margin-tb p-1" scope="col"><a class="btn btn-success" href="{{ route('users.create') }}"> Új felhasználó hozzáadása</a></th>
+            </tr>
+        </thead>
         @if ($message = Session::get('success'))
             <div class="alert alert-success">
                 <p>{{ $message }}</p>
             </div>
         @endif
-       
-        <table class="table table-bordered">
-            <tr>
-                <th>Felhasználónév</th>
-                <th>Jogosultság</th>
-                <th>Művelet</th>
+        <tbody>
+            <tr class="row">
+                <th class="col-lg-4 col-md-4 col-sm-12 text-center" scope="row">Felhasználónév</th>
+                <th class="col-lg-4 col-md-4 col-sm-12 text-center" scope="row">Szerepkör</th>
+                <th class="col-lg-4 col-md-4 col-sm-12 text-center" scope="row">Művelet</th>
             </tr>
             @foreach ($users as $user)
-            <tr>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->role->role_name }}</td>
-                <td>
+            <tr class="row">
+                <td class="col-lg-4 col-md-4 col-sm-12 text-center">{{$user->username}}</td>
+                <td class="col-lg-4 col-md-4 col-sm-12 text-center" >{{$user->role->role_name}}</td>
+                <td class="col-lg-4 col-md-4 col-sm-12 text-center">
                     <form action="{{ route('users.destroy',$user->user_id) }}" method="POST">
        
                         <a class="btn btn-info" href="{{ route('users.show',$user->user_id) }}">Részletek</a>
@@ -42,11 +37,11 @@
                         <button type="submit" onclick="return confirm('Biztosan törli a felhasználót?')" class="btn btn-danger">Törlés</button>
                     </form>
                 </td>
-                
             </tr>
             @endforeach
-        </table>
-    </div>
+        </tbody>
+    </table>
+</div>
   
     
       
