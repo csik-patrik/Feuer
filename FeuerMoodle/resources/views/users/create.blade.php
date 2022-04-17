af70f1f853ad1aade8779a9320731716ff528c5b
csik-patrik <csik.patrik455@gmail.com>
2022-04-17 11:49:50 +0200

    Fix insert

--- FeuerMoodle/resources/views/users/create.blade.php
+++ FeuerMoodle/resources/views/users/create.blade.php
@@ -43,7 +43,7 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>Szerepkör:</strong>
-                        <select name="roleId" class="form-control">
+                        <select name="role_id" class="form-control">
                             @foreach ($roles as $role)
                                 <option value="{{$role->role_id}}">{{$role->role_name}}</option>
                             @endforeach
